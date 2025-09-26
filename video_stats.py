e608da5d6bf08f2e11582e32d927c6eb25d4c2a5
tanhy1998 <tanhongyang0206@gmail.com>
2025-09-26 14:19:41 +0800

    created get_video_ids functions to extract the video ids

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 
 def get_playlist_id():
@@ -34,7 +35,47 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
-    
-if __name__ == "__main__":
-    get_playlist_id()
 
+
+def get_video_ids(playlistId) -> list[str] | None:
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try:
+
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url, verify=False)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
